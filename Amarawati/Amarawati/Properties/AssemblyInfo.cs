75105545e2aa15d3cf0d52b37dc7f1e32b7bb16e
sanjeev12 <mr.sanjeevjarga@gmail.com>
2015-07-10 12:57:30 +0545

    new

--- Amarawati/Amarawati/Properties/AssemblyInfo.cs
+++ Amarawati/Amarawati/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WindowsFormsApplication17")]
+[assembly: AssemblyTitle("Amarawati")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WindowsFormsApplication17")]
+[assembly: AssemblyProduct("Amarawati")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("60890e8e-0877-4d09-b248-02dddb089ec3")]
+[assembly: Guid("3ff1ff02-bec3-47ef-a1df-2165f8fe50f6")]
 
 // Version information for an assembly consists of the following four values:
 //
